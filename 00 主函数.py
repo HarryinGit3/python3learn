01c7186ff2a14e7c4e824d7274fc66611e8e4c82
Harryin <yh821219454@163.com>
2018-02-25 20:41:59 +0800

    添加函数参数相关

--- 00 主函数.py
+++ 00 主函数.py
@@ -1,8 +1,12 @@
 import myfunction
 import sys
-# 设置函数所在路径 然后直接使用
-sys.path.append('C:\\code\\python3\\learn\\python3learn')
-a = myfunction.my_abs(-11)
-x1,x2 = myfunction.quadratic(1, -3, 2)
-print(a)
-print(x1,x2)
+if __name__ =='__main__':
+    # 设置函数所在路径 然后直接使用 主函数都在myfunciton中
+    sys.path.append('C:\\code\\python3\\learn\\python3learn')
+    a = myfunction.my_abs(-11)
+    x1,x2 = myfunction.quadratic(1, -3, 2)
+    print(a)
+    print(x1,x2)
+    nums = [1,3,5]
+    print(myfunction.calc(*nums))
+    print(myfunction.foo(1,2,3,4,5,name='harryin'))
--- myfunction.py
+++ myfunction.py
@@ -22,3 +22,16 @@ def quadratic(a, b, c):
     x1 = (-b + math.sqrt(b * b - 4 * a * c)) / (2 * a)
     x2 = (-b - math.sqrt(b * b - 4 * a * c)) / (2 * a)
     return x1, x2
+
+def calc(*nums):
+    sum = 0
+    for n in nums:
+        sum = sum + n*n
+    return sum
+
+def foo(x,y,z,*args,**kw):
+    print(x)
+    print(y)
+    print(z)
+    print(args)
+    print(kw)
