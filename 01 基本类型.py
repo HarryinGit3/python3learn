7abc6c8e4a068f4ab7f1e900b631e433f87a99ab
Harryin <yh821219454@163.com>
2018-02-25 15:01:23 +0800

    添加元组相关内容

--- 01 基本类型.py
+++ 01 基本类型.py
@@ -43,4 +43,14 @@ classmates.pop() #删除最后一个
 classmates.pop(1)
 print(classmates[-1])
 
-# tuple 元组
+# tuple 元组 元组本身的要素不能更改
+classmates = ('Michael', 'Bob', 'Tracy')
+print('classmates =', classmates)
+print('len(classmates) =', len(classmates))
+print('classmates[0] =', classmates[0])
+print('classmates[1] =', classmates[1])
+print('classmates[2] =', classmates[2])
+print('classmates[-1] =', classmates[-1])
+
+# cannot modify tuple:
+# classmates[0] = 'Adam'
