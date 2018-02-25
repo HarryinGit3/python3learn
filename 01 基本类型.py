fd2c5f97125f7fd58cb135d6bf4ebae544b2969a
Harryin <yh821219454@163.com>
2018-02-25 16:21:37 +0800

    添加dict和set相关内容

--- 01 基本类型.py
+++ 01 基本类型.py
@@ -54,3 +54,38 @@ print('classmates[-1] =', classmates[-1])
 
 # cannot modify tuple:
 # classmates[0] = 'Adam'
+
+
+# 循环 for // while // continue // break
+# 打印list:
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+
+# 打印数字 0 - 9
+for x in range(10):
+    print(x)
+
+sum = 0
+n = 1
+while n <= 100:
+    sum = sum + n
+    n = n + 1
+print(sum)
+
+# dict和set
+d = {
+    'Michael': 95,
+    'Bob': 75,
+    'Tracy': 85
+}
+print('d[\'Michael\'] =', d['Michael'])
+print('d[\'Bob\'] =', d['Bob'])
+print('d[\'Tracy\'] =', d['Tracy'])
+print('d.get(\'Thomas\', -1) =', d.get('Thomas', -1))
+
+s1 = set([1, 1, 2, 2, 3, 3])
+print(s1)
+s2 = set([2, 3, 4])
+print(s1 & s2)
+print(s1 | s2)
