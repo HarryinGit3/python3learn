88d11835373ab08163c31467c1ba5e5a090722d5
Harryin <yh821219454@163.com>
2018-02-26 11:27:47 +0800

    代码对齐

--- 01 基本类型.py
+++ 01 基本类型.py
@@ -1,15 +1,15 @@
 # 整数类型 浮点数没有太多区别
 a = 1
 b = 1.2e-5
-print(a,'\n',b)
+print(a, '\n', b)
 
 # 字符串 用""或者''都可以 \是转义字符  \\就是表示\
 print('I\'m ok')
 print('\\\n\\')
-print(r'\\\n\\') # 用r''表示所有的不使用转义字符
+print(r'\\\n\\')  # 用r''表示所有的不使用转义字符
 print('''line1
 line2
-line3''') # '''多行换行
+line3''')  # '''多行换行
 print(r'''hello,\n
 world''')
 
@@ -23,7 +23,7 @@ else:
 # 变量
 a = 123
 print(a)
-a ='ABC'
+a = 'ABC'
 print(a)
 
 a = 'ABC'
@@ -36,10 +36,10 @@ print('%2d-%02d' % (3, 1))
 print('%.2f' % 3.1415926)
 
 # list是一种数据类型，是列表，可以嵌套
-classmates =['yin','jiang','gao']
-classmates.append('Adam') # 添加到最后一个
+classmates = ['yin', 'jiang', 'gao']
+classmates.append('Adam')  # 添加到最后一个
 classmates.insert(1, 'Jack')
-classmates.pop() #删除最后一个
+classmates.pop()  # 删除最后一个
 classmates.pop(1)
 print(classmates[-1])
 
--- myfunction.py
+++ myfunction.py
@@ -2,6 +2,8 @@ import math
 
 # 内置函数 https://docs.python.org/3/library/functions.html
 # 定义函数 isinstance是用来检查参数类型的
+
+
 def my_abs(x):
     if not isinstance(x, (int, float)):
         raise TypeError('bad operand type')
@@ -9,19 +11,24 @@ def my_abs(x):
         return x
     else:
         return -x
+
+
 def move(x, y, step, angle=0):
     nx = x + step * math.cos(angle)
     ny = y - step * math.sin(angle)
     return nx, ny
 
-if __name__ =='__main__':
-    x, y = move(100, 100, 60, math.pi / 6 )
-    print(x,y)
+
+if __name__ == '__main__':
+    x, y = move(100, 100, 60, math.pi / 6)
+    print(x, y)
+
 
 def quadratic(a, b, c):
     x1 = (-b + math.sqrt(b * b - 4 * a * c)) / (2 * a)
     x2 = (-b - math.sqrt(b * b - 4 * a * c)) / (2 * a)
     return x1, x2
+
 
 def calc(*nums):
     sum = 0
@@ -29,7 +36,8 @@ def calc(*nums):
         sum = sum + n*n
     return sum
 
-def foo(x,y,z,*args,**kw):
+
+def foo(x, y, z, *args, **kw):
     print(x)
     print(y)
     print(z)
